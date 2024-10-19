fd967084ad57e07d243c355fb4e6d5713283776a
xdite <xdite@hey.com>
2024-10-19 21:49:34 +0800

    ## Commit Message
    修复了支付方法管理器中的一些问题 [T-123] [Payment method manager]
    
    优化了代码逻辑，提高性能
    
    - 添加了新文件以改善项目结构
    - 更新了 README 和 index.html 文件
    - 优化了 TypeScript 配置和实现
    - 添加了新的依赖包以增强功能
    - 可能存在的问题：新数据格式可能与旧版本不兼容

---
+++ README.md
@@ -0,0 +1,17 @@
+# Logseq Plugin Template
+
+
+
+
+
+## 開發
+
+歡迎提交 Issues 和 Pull Requests 來幫助改進這個插件！
+
+## 許可證
+
+MIT
+
+## 聯繫方式
+
+如有任何問題或建議，請通過 [GitHub Issues](https://github.com/xdite/logseq-plugin-template/issues) 聯繫我們。
---
+++ index.ts
@@ -0,0 +1,13 @@
+import '@logseq/libs'
+import { saveAudioToAssets } from './utils'
+import { settingsSchema } from './settings'
+
+/**
+ * main entry
+ */
+async function main() {
+
+}
+
+// bootstrap
+logseq.ready(main).catch(console.error)
